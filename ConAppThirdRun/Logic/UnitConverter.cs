a70d5a4fad57361e0f280cb2acfdad6230c34594
kriss3 <kris_s2@outlook.com>
2015-02-25 12:59:16 -0800

    Adding extra info to methods in UnitConverter

--- ConAppThirdRun/Logic/UnitConverter.cs
+++ ConAppThirdRun/Logic/UnitConverter.cs
@@ -5,16 +5,31 @@ using System.Text;
 
 namespace ConAppThirdRun.Logic
 {
+	/// <summary>
+	/// Class to provide the means of representing different metric systems i.e.
+	/// converter between inches and mm and vice-versa
+	/// this is hypthetical for different markets e.g. Canada/USA
+	/// </summary>
 	public class UnitConverter
 	{
 		private const double MILLIMETERS_PER_INCH = 25.4;
 		private double _millimeters;
 
+		/// <summary>
+		/// Converts milimeters to inches
+		/// </summary>
+		/// <param name="mm">millimeter value</param>
+		/// <returns>UnitConverter with value translated to inches</returns>
 		public static UnitConverter ToInches(double mm)
 		{
 			 return new UnitConverter { _millimeters = mm };
 		}
 
+		/// <summary>
+		/// Converts inches to millimeters
+		/// </summary>
+		/// <param name="inch">Value to be converted to milimeters</param>
+		/// <returns>UnitConverter containing value in millimeters</returns>
 		public static UnitConverter ToMillimeters(double inch)
 		{
 			 return new UnitConverter { _millimeters = inch * MILLIMETERS_PER_INCH };
